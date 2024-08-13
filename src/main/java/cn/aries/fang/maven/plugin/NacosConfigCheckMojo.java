3d9b7f863a99980b76b7fa6d30c161e7621094bc
aries.fang <aries.fang.nj@gmail.com>
2024-08-13 15:00:22 +0800

    支持多个配置文件同时检查

--- src/main/java/cn/aries/fang/maven/plugin/NacosConfigCheckMojo.java
+++ src/main/java/cn/aries/fang/maven/plugin/NacosConfigCheckMojo.java
@@ -1,17 +1,21 @@
 package cn.aries.fang.maven.plugin;
 
 
+import cn.aries.fang.maven.plugin.dto.MappingConfig;
 import cn.aries.fang.maven.plugin.utils.HttpClientResult;
 import cn.aries.fang.maven.plugin.utils.HttpClientUtils;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.json.JSONUtil;
 import cn.aries.fang.maven.plugin.dto.NacosLoginResp;
+
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
@@ -37,30 +41,86 @@ public class NacosConfigCheckMojo extends AbstractMojo {
     @Parameter(property = "nacos.namespace", defaultValue = "")
     private String namespace;
 
-    @Parameter(property = "nacos.dataId", required = true)
-    private String dataId;
+    @Parameter(property = "configs", required = true)
+    private List<MappingConfig> configs;
 
-    @Parameter(property = "nacos.group", defaultValue = "DEFAULT_GROUP")
-    private String group;
 
-    @Parameter(property = "configPath", required = true)
-    private String configPath;
-
-    public void execute() throws MojoExecutionException {
+    public void execute() throws MojoExecutionException, MojoFailureException {
 
         getLog().info("=========门禁检查启动=========");
+
+        // 获取Nacos的token
+        String nacosToken = getNacosToken();
+        // 检查配置
+        Map<String, List<String>> result = new HashMap<>();
+        for (MappingConfig config : configs) {
+            List<String> errorKeys = handleMappingConfig(config, nacosToken);
+            if (CollectionUtil.isNotEmpty(errorKeys)) {
+                result.put(config.getDataId(), errorKeys);
+            }
+        }
+        // 输出结果
+        if (CollectionUtil.isEmpty(result.keySet())) {
+            getLog().info("=========门禁检查通过=========");
+        } else {
+            getLog().error("=========门禁检查不通过=========");
+            StringBuilder sb = new StringBuilder();
+            result.forEach((key, value) -> {
+                String errorMessage = key + "以下key缺失：\n" + String.join(",", value);
+                sb.append(errorMessage);
+                getLog().error(errorMessage);
+            });
+            throw new MojoExecutionException(sb.toString());
+        }
+    }
+
+    /**
+     * 处理单个配置文件
+     *
+     * @param config 配置文件信息
+     * @throws MojoExecutionException 执行异常
+     */
+    private List<String> handleMappingConfig(MappingConfig config, String nacosToken) throws MojoExecutionException {
         Yaml yaml = new Yaml();
         Map<String, Object> localYaml = null;
         List<String> localYamlKeys = new ArrayList<>();
-        getLog().info("=========加载本地文件："+configPath+"=========");
-        try (InputStream inputStream = FileUtil.getInputStream(new File(configPath))) {
+        getLog().info("=========加载本地文件：" + config.getConfigPath() + "=========");
+        try (InputStream inputStream = FileUtil.getInputStream(new File(config.getConfigPath()))) {
             localYaml = yaml.load(inputStream);
             extractKeys(localYaml, localYamlKeys, "");
         } catch (Exception e) {
             getLog().error("=========门禁检查不通过=========");
-            throw new MojoExecutionException("加载" + configPath + "失败", e);
+            throw new MojoExecutionException("加载" + config.getConfigPath() + "失败", e);
         }
 
+        String url = String.format("%s/nacos/v1/cs/configs?dataId=%s&group=%s&tenant=%s&accessToken=%s", serverUrl, config.getDataId(), config.getGroup(), namespace, nacosToken);
+        Map<String, Object> nacosYaml = null;
+        List<String> nacosYamlKeys = new ArrayList<>();
+        getLog().info("=========加载远程文件：namespace：" + namespace + " dataId：" + config.getDataId() + "=========");
+        try {
+            // 发送 GET 请求并获取响应
+            HttpClientResult response = HttpClientUtils.doGet(url);
+            if (response.getCode() == 200) {
+                nacosYaml = yaml.load(response.getContent());
+                extractKeys(nacosYaml, nacosYamlKeys, "");
+            } else {
+                getLog().error("=========门禁检查不通过=========");
+                throw new MojoExecutionException("下载nacos文件失败，dataId:" + config.getDataId() + "，HTTP Status: " + response.getCode() + "，content:" + response.getContent());
+            }
+        } catch (Exception e) {
+            getLog().error("=========门禁检查不通过=========");
+            throw new MojoExecutionException("下载nacos文件失败，dataId:" + config.getDataId(), e);
+        }
+        return nacosYamlKeys.stream().filter(key -> !localYamlKeys.contains(key)).collect(Collectors.toList());
+    }
+
+    /**
+     * 获取nacos token
+     *
+     * @return token
+     * @throws MojoExecutionException 异常
+     */
+    private String getNacosToken() throws MojoExecutionException {
         // 登录nacos
         String nacosPwd = decryptPassword(encryptPassword, password);
         String loginUrl = String.format("%s/nacos/v1/auth/users/login", serverUrl);
@@ -84,47 +144,17 @@ public class NacosConfigCheckMojo extends AbstractMojo {
             getLog().error("=========门禁检查不通过=========");
             throw new MojoExecutionException("登录nacos失败：", e);
         }
-
-        String url = String.format("%s/nacos/v1/cs/configs?dataId=%s&group=%s&tenant=%s&accessToken=%s", serverUrl,
-                dataId, group, namespace, accessToken);
-        Map<String, Object> nacosYaml = null;
-        List<String> nacosYamlKeys = new ArrayList<>();
-        getLog().info("=========加载远程文件：namespace：" + namespace + " dataId：" + dataId + "=========");
-        try {
-            // 发送 GET 请求并获取响应
-            HttpClientResult response = HttpClientUtils.doGet(url);
-            if (response.getCode() == 200) {
-                nacosYaml = yaml.load(response.getContent());
-                extractKeys(nacosYaml, nacosYamlKeys, "");
-            } else {
-                getLog().error("=========门禁检查不通过=========");
-                throw new MojoExecutionException(
-                        "下载nacos文件失败，dataId:" + dataId + "，HTTP Status: " + response.getCode() + "，content:"
-                                + response.getContent());
-            }
-        } catch (Exception e) {
-            getLog().error("=========门禁检查不通过=========");
-            throw new MojoExecutionException("下载nacos文件失败，dataId:" + dataId, e);
-        }
-
-        List<String> errorKeys = nacosYamlKeys.stream().filter(key -> !localYamlKeys.contains(key))
-                .collect(Collectors.toList());
-        if (CollectionUtil.isEmpty(errorKeys)) {
-            getLog().info("=========门禁检查通过=========");
-        } else {
-            getLog().error("=========门禁检查不通过=========");
-            getLog().error("以下key缺失：\n" + String.join(",", errorKeys));
-            throw new MojoExecutionException("以下key缺失：\n" + String.join(",", errorKeys));
-        }
+        return accessToken;
     }
 
     /**
      * 递归方法，提取所有的key
-     * @param map  nacos的yaml
-     * @param keys 提取的key集合
+     *
+     * @param map       nacos的yaml
+     * @param keys      提取的key集合
      * @param parentKey 父key
      */
-    private static void extractKeys(Map<String, Object> map, List<String> keys, String parentKey) {
+    private void extractKeys(Map<String, Object> map, List<String> keys, String parentKey) {
         for (Map.Entry<String, Object> entry : map.entrySet()) {
             String currentKey = parentKey.isEmpty() ? entry.getKey() : parentKey + "." + entry.getKey();
             if (entry.getValue() instanceof Map) {
---
+++ src/main/java/cn/aries/fang/maven/plugin/dto/MappingConfig.java
@@ -0,0 +1,34 @@
+package cn.aries.fang.maven.plugin.dto;
+
+public class MappingConfig {
+
+    private String dataId;
+
+    private String group = "DEFAULT_GROUP";
+
+    private String configPath;
+
+    public String getDataId() {
+        return dataId;
+    }
+
+    public void setDataId(String dataId) {
+        this.dataId = dataId;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getConfigPath() {
+        return configPath;
+    }
+
+    public void setConfigPath(String configPath) {
+        this.configPath = configPath;
+    }
+}
